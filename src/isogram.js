6d2f4a11eb13b5470beca7805516799ccf2a7bf6
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-04 10:12:48 +0200

    Final JS solution + TS solution + clever solution

--- src/isogram.js
+++ src/isogram.js
@@ -1,14 +1,29 @@
 function isogram(str = '') {
 
+  if (typeof(str) !== 'string') return false;
   str = str.toLowerCase();
 
   for (let i = 0; i < str.length; i++){
     const letter = str[i];
-    const wordSlice = str.slice([i+1])
+    const wordSlice = str.slice(i+1)
     if (wordSlice.indexOf(letter) !== -1) return false
   }
   
   return true;
+
+
+  // return !str || str.length === new Set(str.toLocaleLowerCase()).size
+
+  // if (typeof(str) !== 'string') return false;
+  // str = str.toLowerCase();
+
+  // for (let i = 0; i < str.length; i++){
+  //   const letter: string = str[i];
+  //   const wordSlice: string = str.slice(i+1)
+  //   if (wordSlice.indexOf(letter) !== -1) return false
+  // }
+  
+  // return true;
 }
 
 module.exports = isogram;
